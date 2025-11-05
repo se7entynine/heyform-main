3bd7d24fa8bf4ec7a34b7ebcfacd067ccf7a9d05
julius <git@internetzi.de>
2025-11-05 06:53:44 +0100

    NoBr-NoCSS

--- packages/webapp/src/pages/form/views/FormComponents/views/Branding.tsx
+++ packages/webapp/src/pages/form/views/FormComponents/views/Branding.tsx
@@ -4,11 +4,5 @@ import { LogoIcon } from '../components'
 import { useTranslation } from '../utils'
 
 export const Branding: FC = () => {
-  const { t } = useTranslation()
-
-  return (
-    <a className="heyform-branding" href="https://heyform.net/?ref=badge" target="_blank">
-      <LogoIcon /> {t('Made with')} HeyForm
-    </a>
-  )
+  return null
 }
